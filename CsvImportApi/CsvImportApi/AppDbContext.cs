5e2e24cd0cf10e9ac3c98b7f7fffc3d0587d4a6d
ivan <ivan20140767@gmail.com>
2025-08-14 19:02:32 +0300

    Создание модели и контекста

---
+++ CsvImportApi/CsvImportApi/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CsvImportApi.Models; // Мои модели
+
+
+namespace CsvImportApi
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Result> Results { get; set; }
+    }
+}
---
+++ CsvImportApi/CsvImportApi/Models/Results.cs
@@ -0,0 +1,14 @@
+﻿namespace CsvImportApi.Models
+{
+    public class Result
+    {
+        public int Id { get; set; } // первичный ключ
+        public double DeltaDateSeconds { get; set; }
+        public DateTime MinDate { get; set; }
+        public double ExecutionTime { get; set; }
+        public double AvgValue {get; set;}
+        public double MedianValue {get; set;}
+        public double MaxValue { get; set;} 
+        public double minValue { get; set;}
+    }
+}
