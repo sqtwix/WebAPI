c213e734a4dd1878f7e6e5347187536df575ceaf
sqtwix <ivan20140767@gmail.com>
2025-08-16 02:46:01 +0300

    Сработовшая миграция

---
+++ CsvImportApi/CsvImportApi/Controllers/CsvImportController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CsvImportApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CsvImportController : ControllerBase
+    {
+        private readonly AppDbContext _context; // контекст
+
+        public CsvImportController (AppDbContext context) // конструктор
+        {
+            _context = context;
+        }
+        
+    }
+}
---
+++ CsvImportApi/CsvImportApi/Migrations/20250815225344_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CsvImportApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DeltaDateSeconds = table.Column<double>(type: "double precision", nullable: false),
+                    MinDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ExecutionTime = table.Column<double>(type: "double precision", nullable: false),
+                    AvgValue = table.Column<double>(type: "double precision", nullable: false),
+                    MedianValue = table.Column<double>(type: "double precision", nullable: false),
+                    MaxValue = table.Column<double>(type: "double precision", nullable: false),
+                    minValue = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Results");
+        }
+    }
+}
